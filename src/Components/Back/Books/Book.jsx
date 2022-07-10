84865330caaf94a2f31d12e3264475682c6637bc
julijajarmale <julija.jarmale@gmail.com>
2022-07-10 21:56:44 +0300

    books

--- src/Components/Back/Books/Book.jsx
+++ src/Components/Back/Books/Book.jsx
@@ -20,17 +20,18 @@ function Author({book}) {
           <li className="book-list-item">
             
                 <div className="content">
+                <div className="book-item">
+                {
+                        book.photo ? <div className="book-cover"><img src={book.photo} alt={book.title} /></div> : null
+                    }
+                    </div>
                     <span className="book-item">{book.title}</span>
                     <span className="book-item">{book.price}</span>
                     <span className="book-item">{book.price.toFixed(2)}</span>
                     <span className="book-item">{book.authorname} {book.authorsurname}</span>
                     <span className="book-item">{new Date(Date.parse(book.date)).toLocaleString()}</span>
                 </div>
-                <div className="book-item">
-                {
-                        book.photo ? <div className="book-cover"><img src={book.photo} alt={book.title} /></div> : null
-                    }
-                    </div>
+               
                 <div className="buttons">
                     <button type="button" className="buttons btn2" onClick={handleEdit}>Edit</button>
                     <button type="button" className="buttons btn3" onClick={handleDelete}>Delete</button>
