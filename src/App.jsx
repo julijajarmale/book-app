49f9f780efaa12e75b6552bd57f62e30faddcc07
julijajarmale <julija.jarmale@gmail.com>
2022-07-11 15:29:42 +0300

    login

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,26 @@
 import './Layout.scss';
 import './crud.scss';
 import './App.scss';
-
-
 import {
     BrowserRouter,
     Routes,
     Route,
+    Navigate,
+    useNavigate,
 } from "react-router-dom";
 import Front from './Components/Front/Front';
 import Back from './Components/Back/Back';
+import { login, logout, authConfig } from './Functions/auth';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
   return (
     <BrowserRouter>
     <Routes> 
-        <Route path="/" element={<Front/>} />
+        <Route path="/" element={<RequireAuth role="user"><Front/></RequireAuth>} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/logout" element={<LogoutPage />} />
         <Route path="/admin" element={<Back show="admin"/>} /> 
         <Route path="/admin/authors" element={<Back show="authors"/>} />
         <Route path="/admin/books" element={<Back show="books"/>} />
@@ -24,5 +29,55 @@ function App() {
     </BrowserRouter>
 )
 }
+function RequireAuth({ children,role}) {
+    const [view, setView] = useState(<h2>Please wait...</h2>);
+  
+    useEffect(() => {
+      axios.get('http://localhost:3003/login-check?role=' + role, authConfig())
+        .then(res => {
+          if ('ok' === res.data.msg) {
+            setView(children);
+          } else {
+            setView(<Navigate to="/login" replace />);
+          }
+        })
+  
+    }, [children, role]);
+  
+    return view;
+  }
 
+  
+  
+  function LoginPage() {
+    const navigate = useNavigate();
+  
+    const [user, setUser] = useState('');
+    const [pass, setPass] = useState('');
+  
+    const doLogin = () => {
+      axios.post('http://localhost:3003/login', { user, pass })
+        .then(res => {
+          console.log(res.data);
+          if ('ok' === res.data.msg) {
+            login(res.data.key);
+            navigate('/', { replace: true });
+          }
+        })
+    }
+    return (
+      <div>
+        <div>name: <input type="text" value={user} onChange={e => setUser(e.target.value)}></input></div>
+        <div>password: <input type="password" value={pass} onChange={e => setPass(e.target.value)}></input></div>
+        <button onClick={doLogin}>Login</button>
+      </div>
+    );
+  }
+  
+  function LogoutPage() {
+    useEffect(() => logout(), []);
+    return (
+      <Navigate to="/login" replace />
+    )
+  }
 export default App;
